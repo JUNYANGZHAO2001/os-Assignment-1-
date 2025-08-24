43e27a1ddc8f110d409bdbaa222f9119c46f837b
JUNYANGZHAO2001 <140377826+JUNYANGZHAO2001@users.noreply.github.com>
2025-08-24 09:47:28 +0930

    Update minishell.c

--- minishell.c
+++ minishell.c
@@ -16,8 +16,8 @@
 #include <stdlib.h>
 #include <errno.h>
 
-#define NV 20
-#define NL 100
+#define NV 64
+#define NL 256
 
 char line[NL];
 
@@ -31,11 +31,13 @@ Job jobs[100];
 int job_count = 0;
 int next_job_id = 1;
 
+/* shell prompt */
 void prompt(void) {
-    printf("\nmsh> ");
+    printf("msh> ");
     fflush(stdout);
 }
 
+/* check background jobs for completion */
 void check_background_jobs(void) {
     int status;
     for (int i = 0; i < job_count; i++) {
@@ -50,6 +52,7 @@ void check_background_jobs(void) {
     }
 }
 
+/* reconstruct full command string from argv */
 void build_command_string(char *dest, char *argv[]) {
     dest[0] = '\0';
     for (int i = 0; argv[i] != NULL; i++) {
@@ -58,11 +61,8 @@ void build_command_string(char *dest, char *argv[]) {
     }
 }
 
-int main(int argk, char *argv[], char *envp[]) {
-    int frkRtnVal;
+int main(void) {
     char *v[NV];
-    char *sep = " \t\n";
-    int i;
 
     while (1) {
         prompt();
@@ -72,20 +72,22 @@ int main(int argk, char *argv[], char *envp[]) {
             continue;
         }
 
-        if (line[0] == '#' || line[0] == '\n' || line[0] == '\0') {
+        if (line[0] == '#' || line[0] == '\n') {
             check_background_jobs();
             continue;
         }
 
-        v[0] = strtok(line, sep);
+        /* tokenize input */
+        v[0] = strtok(line, " \t\n");
+        int i;
         for (i = 1; i < NV; i++) {
-            v[i] = strtok(NULL, sep);
-            if (v[i] == NULL) break;
+            v[i] = strtok(NULL, " \t\n");
+            if (!v[i]) break;
         }
 
-        if (v[0] == NULL) continue;
+        if (!v[0]) continue;
 
-        // built-in cd
+        /* built-in cd */
         if (strcmp(v[0], "cd") == 0) {
             char *dir = v[1] ? v[1] : getenv("HOME");
             if (chdir(dir) == -1) {
@@ -95,33 +97,33 @@ int main(int argk, char *argv[], char *envp[]) {
             continue;
         }
 
-        // check background
+        /* check background command */
         int background = 0;
         if (i > 0 && v[i-1] && strcmp(v[i-1], "&") == 0) {
             background = 1;
-            v[i-1] = NULL;
+            v[i-1] = NULL; // remove &
         }
 
-        frkRtnVal = fork();
-        if (frkRtnVal < 0) {
+        pid_t pid = fork();
+        if (pid < 0) {
             perror("fork");
             continue;
         }
 
-        if (frkRtnVal == 0) { // child
+        if (pid == 0) { // child
             execvp(v[0], v);
-            perror("execvp");
+            perror("execvp"); // exec failed
             _exit(1);
         } else { // parent
             if (background) {
                 jobs[job_count].job_id = next_job_id++;
-                jobs[job_count].pid = frkRtnVal;
+                jobs[job_count].pid = pid;
                 build_command_string(jobs[job_count].command, v);
-                printf("[%d] %d\n", jobs[job_count].job_id, frkRtnVal);
+                printf("[%d] %d\n", jobs[job_count].job_id, pid);
                 fflush(stdout);
                 job_count++;
             } else {
-                if (waitpid(frkRtnVal, NULL, 0) == -1) {
+                if (waitpid(pid, NULL, 0) == -1) {
                     perror("waitpid");
                 }
             }
