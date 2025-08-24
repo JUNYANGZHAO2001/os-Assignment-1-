6ed0663505a4ffd21756902fe262bade52ffff4a
JUNYANGZHAO2001 <140377826+JUNYANGZHAO2001@users.noreply.github.com>
2025-08-24 09:42:39 +0930

    Update minishell.c

--- minishell.c
+++ minishell.c
@@ -1,12 +1,11 @@
 /*********************************************************************
-   Program  : miniShell                   
+   Program  : miniShell                   Version    : 1.3
  --------------------------------------------------------------------
-   Modified minishell with:
-   - Background processes (&)
-   - Reporting job completion
-   - Proper "cd" handling
-   - perror after syscalls
-   - exec failure handling
+   skeleton code for linix/unix/minix command line interpreter
+ --------------------------------------------------------------------
+   File			: minishell.c
+   Compiler/System	: gcc/linux
+
 ********************************************************************/
 
 #include <sys/types.h>
@@ -16,126 +15,76 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <signal.h>
-#include <errno.h>
 
-#define NV 20    /* max number of command tokens */
-#define NL 100   /* input buffer size */
+#define NV 20			/* max number of command tokens */
+#define NL 100			/* input buffer size */
+char            line[NL];	/* command input buffer */
 
-char line[NL];   /* command input buffer */
-
-typedef struct {
-    int job_id;
-    pid_t pid;
-    char command[NL];
-} Job;
-
-Job jobs[100];
-int job_count = 0;
-int next_job_id = 1;
 
 /*
-  shell prompt
-*/
-void prompt(void) {
-    fprintf(stdout, "\n msh> ");
-    fflush(stdout);
+	shell prompt
+ */
+
+void prompt(void)
+{
+  // ## REMOVE THIS 'fprintf' STATEMENT BEFORE SUBMISSION
+  fprintf(stdout, "\n msh> ");
+  fflush(stdout);
 }
 
-/* check if background jobs finished */
-void check_background_jobs() {
-    int status;
-    pid_t pid;
-    for (int i = 0; i < job_count; i++) {
-        if (jobs[i].pid != 0) {
-            pid = waitpid(jobs[i].pid, &status, WNOHANG);
-            if (pid > 0) {
-                printf("[%d]+ Done                 %s\n",
-                       jobs[i].job_id,
-                       jobs[i].command);
-                fflush(stdout);
-                jobs[i].pid = 0; // mark as done
-            }
-        }
+
+/* argk - number of arguments */
+/* argv - argument vector from command line */
+/* envp - environment pointer */
+int main(int argk, char *argv[], char *envp[])
+{
+   int             frkRtnVal;	    /* value returned by fork sys call */
+  char           *v[NV];	        /* array of pointers to command line tokens */
+  char           *sep = " \t\n";  /* command line token separators    */
+  int             i;		          /* parse index */
+
+    /* prompt for and process one command line at a time  */
+
+  while (1) {			/* do Forever */
+    prompt();
+    fgets(line, NL, stdin);
+    fflush(stdin);
+
+    // This if() required for gradescope
+    if (feof(stdin)) {		/* non-zero on EOF  */
+      exit(0);
     }
-}
-
-/* reconstruct command string from argv[] */
-void build_command_string(char *dest, char *argv[]) {
-    dest[0] = '\0';
-    for (int i = 0; argv[i] != NULL; i++) {
-        strcat(dest, argv[i]);
-        if (argv[i+1] != NULL) strcat(dest, " ");
+    if (line[0] == '#' || line[0] == '\n' || line[0] == '\000'){
+      continue;			/* to prompt */
     }
-}
 
-int main(int argk, char *argv[], char *envp[]) {
-    int frkRtnVal;      /* value returned by fork sys call */
-    char *v[NV];        /* array of pointers to command line tokens */
-    char *sep = " \t\n";/* command line token separators    */
-    int i;
-
-    while (1) { /* do Forever */
-        prompt();
-        if (fgets(line, NL, stdin) == NULL) {
-            if (feof(stdin)) exit(0);
-            continue;
-        }
-
-        if (line[0] == '#' || line[0] == '\n' || line[0] == '\0') {
-            check_background_jobs();
-            continue;
-        }
-
-        v[0] = strtok(line, sep);
-        for (i = 1; i < NV; i++) {
-            v[i] = strtok(NULL, sep);
-            if (v[i] == NULL) break;
-        }
-
-        if (v[0] == NULL) continue;
-
-        /* handle "cd" */
-        if (strcmp(v[0], "cd") == 0) {
-            char *dir = v[1] ? v[1] : getenv("HOME");
-            if (chdir(dir) == -1) {
-                perror("cd");
-            }
-            check_background_jobs();
-            continue;
-        }
-
-        /* check if last arg is & */
-        int background = 0;
-        if (i > 0 && v[i-1] && strcmp(v[i-1], "&") == 0) {
-            background = 1;
-            v[i-1] = NULL; // remove "&" from args
-        }
-
-        /* fork */
-        frkRtnVal = fork();
-        if (frkRtnVal == -1) {
-            perror("fork");
-            continue;
-        }
-        if (frkRtnVal == 0) { /* child */
-            execvp(v[0], v);
-            perror("execvp"); /* exec failed */
-            exit(1);
-        } else { /* parent */
-            if (background) {
-                jobs[job_count].job_id = next_job_id++;
-                jobs[job_count].pid = frkRtnVal;
-                build_command_string(jobs[job_count].command, v);
-                printf("[%d] %d\n", jobs[job_count].job_id, frkRtnVal);
-                fflush(stdout);
-                job_count++;
-            } else {
-                if (waitpid(frkRtnVal, NULL, 0) == -1) {
-                    perror("waitpid");
-                }
-            }
-        }
-        check_background_jobs();
+    v[0] = strtok(line, sep);
+    for (i = 1; i < NV; i++) {
+      v[i] = strtok(NULL, sep);
+      if (v[i] == NULL){
+	      break;
+      }
     }
-}
+    /* assert i is number of tokens + 1 */
+
+    /* fork a child process to exec the command in v[0] */
+    switch (frkRtnVal = fork()) {
+      case -1:			/* fork returns error to parent process */
+      {
+	      break;
+      }
+      case 0:			/* code executed only by child process */
+      {
+	      execvp(v[0], v);
+      }
+      default:			/* code executed only by parent process */
+      {
+      	wait(0);
+        // REMOVE PRINTF STATEMENT BEFORE SUBMISSION
+        printf("%s done \n", v[0]);
+    	  break;
+      }
+    }				/* switch */
+  }				/* while */
+}				/* main */
 
